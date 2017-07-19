5c8ed9991271cf2251e3992275a69919eea5c62a
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2017-07-19 23:21:35 +1000

    Format rust

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,29 @@ use toml::Value;
 use std::error::Error;
 
 fn readConfig() -> Result<String, String> {
-	File::open("./run.toml")
-		.map_err(|err| err.to_string())
-		.and_then(|mut file| {
-			let mut contents = String::new();
-			file.read_to_string(&mut contents)
-				.map_err(|err| err.to_string())
-				.map(|_| contents)
-		})
+    File::open("./run.toml")
+        .map_err(|err| err.to_string())
+        .and_then(|mut file| {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)
+                .map_err(|err| err.to_string())
+                .map(|_| contents)
+        })
 }
 
 fn parseConfig(config: String) -> Result<Value, String> {
-	config.parse::<Value>()
-		.map_err(|e: toml::de::Error| e.to_string())
+    config.parse::<Value>().map_err(
+        |e: toml::de::Error| e.to_string(),
+    )
 }
 
 fn main() {
-	let config = readConfig()
-		.and_then(parseConfig);
+    let config = readConfig().and_then(parseConfig);
 
-	match config {
-		Ok(text) =>
-			println!("{}", text),
+    match config {
+        Ok(text) => println!("{}", text),
 
-		Err(err) =>
-			println!("{}", err),
-	}
+        Err(err) => println!("{}", err),
+    }
 
 }
