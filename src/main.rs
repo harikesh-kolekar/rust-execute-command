5e48bb284099082a3f1d4d1c8bad6698e929edb1
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2017-07-19 20:27:41 +1000

    Read config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn readConfig() -> String {
+	// let mut file = File::open("./run.toml")?;
+	// let mut contents = String::new();
+
+	// file.read_to_string(&mut contents)?;
+
+	let mut file = File::open("./run.toml").expect("Unable to open the file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read the file");
+
+	contents.to_string()
+	// "hello world".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+	let config = readConfig();
+
+	println!("{}", config);
 }
